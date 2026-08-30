db3233caa3ee41e5bda9e7cf09d5855503a7f221
Mohamed Shaheed <64309668+shaheedams@users.noreply.github.com>
2026-08-30 12:25:00 +0530

    Revise About component content and structure
    Updated the About component with new content and awards.

--- src/components/about/page.tsx
+++ src/components/about/page.tsx
@@ -35,6 +35,7 @@ const About = () => {
                         <p className="mb-5">Focused on writing clean, scalable code that significantly improves user engagement and operational efficiency.</p>
                         <h6 className="text-msprimary mb-2">Certification / Awards:</h6>
                         <ul className="list-disc">
+                             <li className="ml-[25px]">Received the <strong>&quot;Google AI Professional & Essentials&quot;</strong> Certificates from Google, Coursera</li>
                             <li className="ml-[25px]">Received the <strong>&quot;Achiever of the Quarter&quot;</strong> Award for best performance, R.R. Donnelley - <strong>(2024, 2026)</strong></li>
                             <li className="ml-[25px]">Received the <strong>&quot;Rising Star of the Year&quot;</strong> Award for outstanding work in the Tracker, R.R. Donnelley - <strong>(2025)</strong></li>
                         </ul>
